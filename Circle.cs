ab6be5a59b4d810187e7fc94294156e18b4e6e64
unknown <viratcontractor97@gmail.com>
2023-02-05 11:43:08 -0500

    Circle file added

---
+++ Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Circle
+{
+    private int radius;
+
+	public Circle()
+	{
+	}
+
+    public Circle()
+    {
+        this.radius = 0;
+    }
+
+    public int GetRadius()
+    {
+        return this.radius;
+    }
+
+    public void SetRadius(int radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetCircumference()
+    {
+        return this.radius;
+    }
+
+    public double GetArea()
+    {
+        return this.radius;
+    }
+}
