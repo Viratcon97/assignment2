5d6afd6075de0222b27128f0f1064530d5cb8fa0
unknown <viratcontractor97@gmail.com>
2023-02-05 13:06:09 -0500

    Completed console application

---
+++ Assignment2/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Circle
+{
+    public int radius { get; set; }
+
+    public Circle(int radius)
+    {
+        radius = 1;
+    }
+	
+    public Circle()
+    {
+       radius= 1;
+    }
+
+    public int GetRadius()
+    {
+        return radius;
+    }
+
+    public void SetRadius(int radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetCircumference()
+    {
+        double circumference;
+        circumference = 2 * 3.14 * radius;
+        return circumference;
+    }
+
+    public double GetArea()
+    {
+        double area;
+        area = 3.14 * radius * radius;
+        return area;
+    }
+}
--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program{
+    static void Main(string[] args)
+    {
+        int operationNumber;
+        bool flag = true;
+        bool radiusFlag = false;
+
+        Circle circle = new();
+
+
+        while (!radiusFlag)
+        {
+            Console.Write("Please Enter Circle Radius :");
+            int radius = Convert.ToInt16(Console.ReadLine());
+            if (radius > 0) { 
+                radiusFlag= true;
+                circle.SetRadius(radius);
+                flag= false;
+            }
+            else
+            {
+                Console.WriteLine("Enter valid radius value");
+            }
+        }
+
+        
+        while (!flag) 
+        { 
+            try
+            {
+                Console.WriteLine("\n=========================\n");
+                Console.WriteLine("1. Get Circle Radius");
+                Console.WriteLine("2. Change Circle Radius");
+                Console.WriteLine("3. Get Circle Circumference");
+                Console.WriteLine("4. Get Circle Area");
+                Console.WriteLine("5. Exit");
+
+                Console.WriteLine("Please select operation from above :");
+                operationNumber = Convert.ToInt16(Console.ReadLine());
+
+                if (operationNumber == 1)
+                {
+                    getCircleRadius();
+                }
+                else if (operationNumber == 2)
+                {
+                    changeCircleRadius();
+                }
+                else if (operationNumber == 3)
+                {
+                    getCircleCircumference();
+                }
+                else if (operationNumber == 4)
+                {
+                    getCircleArea();
+                } else if (operationNumber == 5)
+                {
+                    flag = true;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        void getCircleRadius()
+        {
+            Console.WriteLine(circle.GetRadius().ToString());
+        }
+
+        void changeCircleRadius()
+        {
+            Console.Write("Please Enter New Circle Radius :");
+            int radius = Convert.ToInt16(Console.ReadLine());
+            circle.SetRadius(radius);
+        }
+
+        void getCircleCircumference()
+        {
+            Console.WriteLine(circle.GetCircumference().ToString());
+        }
+        
+        void getCircleArea()
+        {
+            Console.WriteLine(circle.GetArea().ToString());
+        }
+    }
+}
